65cb63c7a2610853f56040fbea14a220d16e6b5f
vikto <viktor.samokovsky@gmail.com>
2023-12-04 19:20:09 +0900

    Детектор на базе Yolov4.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Ку')
+import cv2
+from modules.detector import CarsDetection
+
+cd = CarsDetection()
+img = cd.detect_car(cv2.imread("images/test1.jpg"))
+
+cv2.imshow("Cars", img)
+cv2.waitKey(0)
