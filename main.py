4325d9a0781d803f6122f50e061a130a632eb464
vikto <viktor.samokovsky@gmail.com>
2023-12-07 17:56:39 +0900

    Добавлен функционал http-сервера.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import cv2
 from modules.detector import CarsDetection
+from flask import Flask, request, jsonify
+import base64
+import numpy as np
 
-cd = CarsDetection()
-img = cd.detect_car(cv2.imread("images/test1.jpg"))
+app = Flask(__name__)
 
-cv2.imshow("Cars", img)
-cv2.waitKey(0)
+
+@app.route('/car/detection', methods=['POST'])
+def detect_car():
+    if 'image' not in request.json:
+        return jsonify({'status': 'error', 'car_detected': False}), 400
+
+    cd = CarsDetection()
+    img, count = cd.detect_car(
+        cv2.imdecode(
+            np.frombuffer(
+                base64.b64decode(request.json['image']), np.uint8
+            ), cv2.IMREAD_UNCHANGED
+        )
+    )
+
+    if count != 0:
+        return jsonify({'status': 'success', 'car_detected': True})
+    return jsonify({'status': 'success', 'car_detected': False})
+
+
+app.run(host='0.0.0.0', port=8080)
--- modules/detector.py
+++ modules/detector.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 class CarsDetection:
 
     def __init__(self):
@@ -7,18 +8,16 @@ class CarsDetection:
         self.model = cv2.dnn_DetectionModel(net)
         self.model.setInputParams(size=(832, 832), scale=1 / 255)
         self.classes_allowed = [2, 3, 5, 6, 7]
+
     def detect_car(self, img):
         vehicles_boxes = []
         class_ids, scores, boxes = self.model.detect(img, nmsThreshold=0.4)
         for class_id, score, box in zip(class_ids, scores, boxes):
             if score < 0.5:
                 continue
-
             if class_id in self.classes_allowed:
                 vehicles_boxes.append(box)
         for box in vehicles_boxes:
             x, y, w, h = box
-
             cv2.rectangle(img, (x, y), (x + w, y + h), (30, 30, 255), 1)
-
-        return img
+        return img, len(vehicles_boxes)
